c1ed4fe4cd9d9fe1ce1e3716191d6516f2d20807
Amey Parundekar <amey.parundekar@gmetri.com>
2020-06-15 19:03:33 +0530

    Fixed local env

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -268,7 +268,7 @@ app.extend({
 		if (!config.web_direct_connect) {
 			url = this.proto + location.host;
 		}
-		if (process.env.IS_LOCAL && config.web_socket_hostname) {
+		if (config.web_socket_hostname) {
 			url = this.proto + config.web_socket_hostname;
 		}
 		Debug.trace("Websocket Connect: " + url);
